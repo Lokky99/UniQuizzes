45e0840d061471e8bd967619d67ee9c83db7c624
dag7dev <dag7@protonmail.com>
2022-02-01 20:35:44 +0000

    Update from https://github.com/dag7dev/UniQuizzes/commit/aa2fa09197eff9192c8f2a8c4fa380330eb8eac4

--- js/config.js
+++ js/config.js
@@ -6,6 +6,7 @@ var rightAnswerPoints = 2
 var wrongAnswerPoints = 1
 var noAnswerPoints = 0
 var numberOfQuestions = 40 // default number of questions that you want to show
+var title = "UniQuizzes"
 
 // CUSTOMIZABLE PART OF THE PAGE - SHOWS OR HIDES SOME PARTS
 // if a part is disabled, then the related functionality will be disabled
@@ -17,6 +18,7 @@ var showShuffleAnswers = true
 // FILES
 var jsonFolder = "json" // json folder where to load your json files
 var jsonFiles = { // load other quizzes
-    "questions_1.json": "SO1",
-    "questions_2.json": "SO2"
+    "so1.json": "SO1",
+    "so2.json": "SO2",
+    "swInedite.json": "SW - Inedite"
 };
